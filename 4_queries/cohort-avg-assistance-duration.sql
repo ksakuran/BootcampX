7c8cbb57f9c9e29c561e10947a85bf45ff716247
Kat Nishimura <katherine.nishimura@live.ca>
2023-02-08 00:25:55 +0000

    add teachers, and assistance request queries

---
+++ 4_queries/cohort-avg-assistance-duration.sql
@@ -0,0 +1,8 @@
+SELECT AVG(total_duration) AS average_total_duration
+FROM
+ (SELECT SUM(completed_at-started_at) AS total_duration
+  FROM assistance_requests
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON cohort_id = cohorts.id
+  GROUP BY cohorts.name
+  ORDER BY total_duration) AS total_durations;
