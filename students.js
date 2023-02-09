c8823a55e27e7aa2181985d72dc8459b6874d4b5
Kat Nishimura <katherine.nishimura@live.ca>
2023-02-09 05:38:06 +0000

    add client/server connection with test queries

---
+++ students.js
@@ -0,0 +1,37 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+// cohort  = process.argv[2]
+// numOfRecords = process.agrv[3]
+
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts LIKE '$1%'
+// LIMIT $2;
+// `, [cohort, numOfRecords])
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// });
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
